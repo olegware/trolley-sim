eb750b6a775438bc9366ea5432df25faa58f0809
olegware <129217985+olegware@users.noreply.github.com>
2023-11-06 00:10:55 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 
 # Parameters
 NUM_PEOPLE = 100
@@ -13,83 +12,69 @@ EMOTIONAL_CONTAGION_FACTOR = 0.1
 RANDOM_EVENT_PROBABILITY = 0.05
 RIPPLE_EFFECT_DECAY = 0.1
 
-# Create a society with a Watts-Strogatz small-world network
-society_graph = nx.watts_strogatz_graph(n=NUM_PEOPLE, k=6, p=0.1)
+# Initialize society graph and happiness
+society_graph = nx.barabasi_albert_graph(n=NUM_PEOPLE, m=3)
 happiness = np.random.uniform(0.5, 1.0, NUM_PEOPLE)
 
+# Set edge weights based on initial happiness
+for i, j in society_graph.edges():
+    society_graph[i][j]['weight'] = np.mean([happiness[i], happiness[j]])
 
+# Simulate trolley problem and update happiness and graph weights
 def simulate_trolley_problem():
     global happiness
     # Trolley problem decision
-    group_saved = np.random.choice(NUM_PEOPLE, size=3, replace=False)
+    group_saved = np.random.choice(NUM_PEOPLE, size=int(NUM_PEOPLE * 0.05), replace=False)
     individual_sacrificed = np.random.choice(NUM_PEOPLE)
-
-    # Update happiness levels
-    happiness[group_saved] += DECISION_IMPACT
-    happiness[individual_sacrificed] -= DECISION_IMPACT * 3
-
-    # Ripple effect simulation
+    # Update happiness
+    happiness[group_saved] += 1
+    happiness[individual_sacrificed] -= DECISION_IMPACT * len(group_saved)
+    # Contagion and random events
     for _ in range(SIMULATION_ROUNDS):
+        new_happiness = happiness.copy()
         for i in range(NUM_PEOPLE):
             neighbors = list(society_graph.neighbors(i))
-            happiness[i] += np.sum((happiness[neighbors] - happiness[i]) * EMOTIONAL_CONTAGION_FACTOR)
+            happiness_diff_sum = sum(society_graph[i][n]['weight'] * (happiness[n] - happiness[i]) for n in neighbors)
+            new_happiness[i] += EMOTIONAL_CONTAGION_FACTOR * happiness_diff_sum
             if np.random.rand() < RANDOM_EVENT_PROBABILITY:
-                happiness[i] += np.random.uniform(-RIPPLE_EFFECT_DECAY, RIPPLE_EFFECT_DECAY)
-            happiness[i] = np.clip(happiness[i], 0, 1)
+                new_happiness[i] += np.random.uniform(-RIPPLE_EFFECT_DECAY, RIPPLE_EFFECT_DECAY)
+            new_happiness[i] = np.clip(new_happiness[i], 0, 1)  # Ensure happiness is within bounds
+        happiness = new_happiness
+    # Update weights based on new happiness
+    for i, j in society_graph.edges():
+        society_graph[i][j]['weight'] = np.clip(np.mean([happiness[i], happiness[j]]), 0, 1)
 
-    return happiness
-
-
-# Prepare initial state for comparison
-initial_happiness = happiness.copy()
-
-
-# GUI functions
-def update_plots():
-    # Simulate the trolley problem to update happiness values
-    final_happiness = simulate_trolley_problem()
-
-    # Calculate net change in happiness
-    net_change = np.sum(final_happiness) - np.sum(initial_happiness)
-
-    # Update the bar chart
-    bar_chart_ax.clear()
-    bar_chart_ax.bar(['Initial', 'Final'], [np.sum(initial_happiness), np.sum(final_happiness)],
-                     color=['blue', 'green'])
-    bar_chart_ax.set_ylabel('Total Happiness')
-    bar_chart_ax.set_title('Net Change in Happiness: {:.2f}'.format(net_change))
-    bar_chart_ax.text(1, np.sum(final_happiness), f'{net_change:.2f}', ha='center', va='bottom')
-
-    # Update the network plot
-    network_ax.clear()
-    pos = nx.spring_layout(society_graph)
-    nx.draw(society_graph, pos, ax=network_ax, node_size=50, node_color=happiness, cmap=plt.cm.viridis,
-            edge_color='gray')
-    network_ax.set_title('Society Network Happiness Levels')
-
-    # Redraw the canvas
-    canvas.draw_idle()
-
-
-# Create the main window
+# GUI setup
 root = tk.Tk()
 root.title('Trolley Problem Simulation')
-
-# Create the matplotlib figure and subplots
-fig = Figure(figsize=(12, 6))
-bar_chart_ax = fig.add_subplot(121)
-network_ax = fig.add_subplot(122)
-
-# Create the canvas and place it on the Tkinter window
+fig, (bar_chart_ax, network_ax) = plt.subplots(1, 2, figsize=(12, 6))
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-# Add a button to update the plots
+initial_happiness = happiness.copy()
+
+# Plot update function
+def update_plots():
+    global initial_happiness
+    simulate_trolley_problem()
+    final_happiness = happiness.copy()
+    net_change = final_happiness.sum() - initial_happiness.sum()
+    bar_chart_ax.clear()
+    bar_chart_ax.bar(['Initial', 'Final'], [initial_happiness.sum(), final_happiness.sum()], color=['blue', 'green'])
+    bar_chart_ax.set_ylabel('Total Happiness')
+    bar_chart_ax.set_title(f'Net Change in Happiness: {net_change:.2f}')
+    network_ax.clear()
+    pos = nx.spring_layout(society_graph, weight='weight')
+    nx.draw(society_graph, pos, ax=network_ax, node_size=50, node_color=happiness, cmap=plt.cm.viridis,
+            edge_color='gray', alpha=0.7)
+    network_ax.set_title('Society Network Happiness Levels')
+    canvas.draw_idle()
+    initial_happiness = final_happiness
+
+# GUI buttons and initial plot
 update_button = tk.Button(root, text="Simulate Trolley Problem", command=update_plots)
 update_button.pack(side=tk.BOTTOM)
-
-# Initial plots
 update_plots()
 
-# Display the GUI
+# Start GUI event loop
 root.mainloop()
